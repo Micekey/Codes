0566217ed974028950f3077f38dce85d8470c035
Micekey <109330251+Micekey@users.noreply.github.com>
2022-08-11 11:36:51 +0530

    Updated solution 1 and added solution 2

--- 1.inverse of a number.cpp
+++ 1.inverse of a number.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+// ***************************************   Solution 1   ***********************************************
 
 int main() {
     int n = 81456273;
     int arr[100] = {0};
-    bool on = true;
     int count = 1;
-    while(on)
+    while(n!=0)
     {
         arr[n%10]=count;
         count++;
         n/=10;
-        if (n==0)
-        on = false;
     }
     
     for (int i=count-1 ; i>0;i--)
     cout<< arr[i];
+    
+    return 0;
+}
+
+
+// ***************************************   Solution 2   ***********************************************
+
+int main() {
+    int n = 81456273;
+    int que = 0;
+    int number = 0;
+    int counter = 1;
+    while(n!=0)
+    {
+        que = n%10;
+        number = number+counter*pow(10, que-1);
+        counter++;
+        n/=10;
+    }
+    cout << number ;
+    
+    return 0;
 }
